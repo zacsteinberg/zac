f4b056851ff973dfa70c1a9e99189f7b1c1f87ab
zacsteinberg <156223431+zacsteinberg@users.noreply.github.com>
2024-01-24 19:02:33 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,3 @@
-let myImage = document.querySelector('img');
-
-myImage.onclick = function() {
-  let mySrc = myImage.getAttribute('src');
-  if(mySrc === 'images/firefox-icon.png') {
-    myImage.setAttribute ('src','images/firefox2.png');
-  } else {
-    myImage.setAttribute ('src','images/firefox-icon.png');
-  }
-}
-
 let myButton = document.querySelector('button');
 let myHeading = document.querySelector('h1');
 
@@ -23,12 +12,12 @@ function setUserName() {
 }
 
 if(!localStorage.getItem('name')) {
-    setUserName();
-  } else {
-    let storedName = localStorage.getItem('name');
-    myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
-  }
-  
-  myButton.onclick = function() {
-    setUserName();
-  }
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.innerHTML = 'Welcome to the website, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
